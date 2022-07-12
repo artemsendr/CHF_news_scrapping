222e704c569c1e33ffee848ee003d524277bdaea
Artem Sendr <senluart@yahoo.com>
2022-07-12 19:39:39 +0300

    sceleton

---
+++ Rate_scrapping.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import grequests
+import requests
+
+def get_rate(start_datetime, end_datetime, interval):
+    """
+    gets information about CHFUSD rate within start to end period by inteval
+    :param start_datetime: start date or datetime
+    :param end_datetime: end date or datetime
+    :param interval:
+    :return:
+    """
+    pass
+
+def get_news(start, end):
+    """
+    returns news from start to end dates
+    :param start: start date
+    :param end: end date
+    :return: list of tuples date - news
+    """
+    news_page_links = get_news_pages(start, end)
+    for links in news_page_links:
+        take_news(link)
+
+def main():
+    get_rate(start, end, interval)
+    get_news(start, end)
